6c2b8c40f9c5cf6d34a02cd6340f18a1c2ee16c7
mewgame <poke.brones@gmail.com>
2025-04-22 15:27:04 +0200

    ajouts modif andreas css et produits php

--- pages/produits.php
+++ pages/produits.php
@@ -1,8 +1,49 @@
 <?php
     include "../inc/top.php"
 ?>
-    <section class="produits">
-        <h2> Liste des produits disponibles </h2>
+
+
+    <section class="puffproduct">
+    <h2> Liste des produits disponibles </h2>
+        <article>
+            <img src="../images/cherrycoca.webp" alt="puff1">
+
+            <section class="infos">
+                <h3>Puff Goût cherrycoca</h3>
+                <strong><p>150 €</p></strong>
+                <button class="puffbut" onclick="window.location.href='<?= RACINE ?>pages/produits.php'">ajouter au panier</button>
+            </section>
+        </article>
+
+        <article>
+            <img src="../images/berry.webp" alt="puff2">
+
+            <section class="infos">
+                <h3>Puff Goût berry berry</h3>
+                <strong><p>150 €</p></strong>
+                <button class="puffbut" onclick="window.location.href='<?= RACINE ?>pages/produits.php'">ajouter au panier</button>
+            </section>
+        </article>
+
+        <article>
+            <img src="../images/strawberry.webp" alt="puff3">
+
+            <section class="infos">
+                <h3>Puff Goût strawberry</h3>
+                <strong><p>150 €</p></strong>
+                <button class="puffbut" onclick="window.location.href='<?= RACINE ?>pages/produits.php'">ajouter au panier</button>
+            </section>
+        </article>
+
+        <article>
+            <img src="../images/bluerazz.webp" alt="puff4">
+
+            <section class="infos">
+                <h3>Puff Goût bluerazz</h3>
+                <strong><p>150 €</p></strong>
+                <button class="puffbut" onclick="window.location.href='<?= RACINE ?>pages/produits.php'">ajouter au panier</button>
+            </section>
+        </article>
     </section>
 
 <?php
