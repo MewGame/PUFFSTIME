761ac4fde40042bddc65d68b0f79230dfb9e7213
mewgame <poke.brones@gmail.com>
2025-04-22 11:30:14 +0200

    formulairestylecss

--- pages/inscription.php
+++ pages/inscription.php
@@ -3,12 +3,12 @@
 ?>
     <main>
         <form action="traitement.php" method="post">
-            <input type="text" id="lastname" name="lastname" placeholder="Nom de Famille" maxlenght="100">
-            <input type="text" id="firstname" name="firstname" placeholder="Prénom" maxlenght="100">
-            <input type="text" id="email" name="email" placeholder="Adresse Mail" maxlenght="100">
-            <input type="text" id="city" name="city" placeholder="Ville" maxlenght="100">
-            <input type="text" id="cp" name="cp" placeholder="Code Postal" maxlenght="100">
-            <input type="text" id="tel" name="tel" placeholder="Téléphone" maxlenght="100">
+            <input type="text" id="lastname" name="lastname" placeholder="Nom de Famille" maxlength="100">
+            <input type="text" id="firstname" name="firstname" placeholder="Prénom" maxlength="100">
+            <input type="text" id="email" name="email" placeholder="Adresse Mail" maxlength="100">
+            <input type="text" id="city" name="city" placeholder="Ville" maxlength="100">
+            <input type="text" id="cp" name="cp" placeholder="Code Postal" maxlength="100">
+            <input type="text" id="tel" name="tel" placeholder="Téléphone" maxlength="100">
             <input type="submit">
         </form>
 </main>
