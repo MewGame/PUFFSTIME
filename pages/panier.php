ebcf4958abbe94592cc415a37913c60e2a210d6f
mewgame <poke.brones@gmail.com>
2025-04-22 11:21:47 +0200

    changementpanier

--- pages/panier.php
+++ pages/panier.php
@@ -1,14 +1,13 @@
 <?php
-    include '../structure/connexion.php';
     include "../inc/top.php"
-
-    $id_client = 1;
-
-    $sql = "SELECT p.Nom, p.prix, c.quantité
-        FROM commandes c
-        JOIN produits p ON c.id_produits = p.id
-        WHERE c.id_clients = ?";
+    include '../structure/connexion.php';
     #Normalement je voulais que ça affiche le panier automatiquement dès qu'on va sur le nav du panier mais je comprends pas du tout comment faut faire donc je vous laisse juste le SQL
+    #$id_client = 1;
+    #$sql = "SELECT p.Nom, p.prix, c.quantité
+        #FROM commandes c
+        #JOIN produits p ON c.id_produits = p.id
+        #WHERE c.id_clients = ?";
+
 ?>
 
     <section class="panier">
