4a04c2b36a64c071d22e1518cbe8d712e215daa6
mewgame <poke.brones@gmail.com>
2025-04-22 11:19:44 +0200

    testdutraitementpanierphp

--- pages/panier.php
+++ pages/panier.php
@@ -1,10 +1,25 @@
 <?php
+    include '../structure/connexion.php';
     include "../inc/top.php"
+
+    $id_client = 1;
+
+    $sql = "SELECT p.Nom, p.prix, c.quantité
+        FROM commandes c
+        JOIN produits p ON c.id_produits = p.id
+        WHERE c.id_clients = ?";
+    #Normalement je voulais que ça affiche le panier automatiquement dès qu'on va sur le nav du panier mais je comprends pas du tout comment faut faire donc je vous laisse juste le SQL
 ?>
+
     <section class="panier">
-        <h2> Liste des produits disponibles </h2>
+        <form action="traitement.php" method="post">
+            <input type="number" id="quantité" name="quantité" placeholder="Quantité">
+            <input type="submit">
+        </form>
     </section>
 
 <?php
     include "../inc/bottom.php"
 ?>
+
+
---
+++ pages/traitementpanier.php
@@ -0,0 +1,22 @@
+<?php
+include '../structure/connexion.php';
+
+$id_client = 1;
+
+if (isset($_POST['id_produit']) && isset($_POST['quantite'])) {
+    $id_produit = (int) $_POST['id_produit'];
+    $quantite = (int) $_POST['quantite'];
+
+    if ($check->rowCount() > 0) {
+        $pdo->prepare("UPDATE commandes SET quantité = quantité + ? WHERE id_clients = ? AND id_produits = ?")
+            ->execute([$quantite, $id_client, $id_produit]);
+    } else {
+        $pdo->prepare("INSERT INTO commandes (id_clients, id_produits, quantité) VALUES (?, ?, ?)")
+            ->execute([$id_client, $id_produit, $quantite]);
+    }
+
+    header("Location: panier.php");
+} else {
+    header("Location: pb.php");
+}
+?>
